2e18e70e61f365e7345b3e500501f9e78cc014ea
Simon Rogers <simon.d.rogers@gmail.com>
2020-07-16 07:59:30 +0100

    added number ms1 and ms2 scans to summary script

--- scripts/mzml_summarise.py
+++ scripts/mzml_summarise.py
@@ -27,6 +27,12 @@ def get_summary(mzml_file_path):
     summary['Nscans'] = len(scan_sub)
     summary['Scans per sec'] = len(scan_sub)/(scan_sub[-1].rt_in_seconds - scan_sub[0].rt_in_seconds)
     summary['FileName'] = mzml_file_path.split(os.sep)[-1]
+
+    ms1_scans = list(filter(lambda x: x.ms_level == 1,scan_sub))
+    ms2_scans = list(filter(lambda x: x.ms_level == 2,scan_sub))
+    summary['Nscans_MS1'] = len(ms1_scans)
+    summary['Nscans_MS2'] = len(ms2_scans)
+
     return summary
 
 if __name__ == '__main__':
@@ -37,7 +43,7 @@ if __name__ == '__main__':
 
 
     summaries = []
-    heads = ['FileName','StartRT','EndRT','Nscans','Scans per sec','First MS2 pos','First MS2 block length']
+    heads = ['FileName','StartRT','EndRT','Nscans','Nscans_MS1','Nscans_MS2','Scans per sec','First MS2 pos','First MS2 block length']
     if os.path.isdir(mzml_path):
         print("Extracting mzml from folder")
         file_list = glob.glob(os.path.join(mzml_path,'*.mzML'))

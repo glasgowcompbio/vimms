db69d4b1a3a39efe16789f439c71dffc2d46dfe6
Simon Rogers <simon.d.rogers@gmail.com>
2020-08-31 13:19:20 +0100

    added missing script files

---
+++ vimms/scripts/data_generation.py
@@ -0,0 +1,91 @@
+# data generation script
+import sys
+import argparse
+from loguru import logger
+import numpy as np
+
+
+sys.path.append('..')
+sys.path.append('../..') # if running in this folder
+
+
+from vimms.Common import *
+from vimms.Chemicals import ChemicalMixtureCreator
+from vimms.ChemicalSamplers import DatabaseFormulaSampler, UniformRTAndIntensitySampler, UniformMS2Sampler
+from vimms.MassSpec import IndependentMassSpectrometer
+from vimms.Controller import TopNController, SWATH
+from vimms.Noise import UniformSpikeNoise
+from vimms.Environment import Environment
+from vimms.Utils import write_msp
+
+
+
+DEFAULT_RT_RANGE = (100,500)
+POSITIVE_IONISATION_MODE = "positive"
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Limited dataset creation')
+    parser.add_argument('output_mzml_file', type=str)
+    parser.add_argument('--min_mz', dest='min_mz', default=DEFAULT_MS1_SCAN_WINDOW[0], type=float)
+    parser.add_argument('--max_mz', dest='max_mz', default=DEFAULT_MS1_SCAN_WINDOW[1], type=float)
+    parser.add_argument('--min_rt', dest='min_rt', default=DEFAULT_RT_RANGE[0], type=float)
+    parser.add_argument('--max_rt', dest='max_rt', default=DEFAULT_RT_RANGE[1], type=float)
+    parser.add_argument('--min_ms1_sampling_intensity', dest='min_ms1_sampling_intensity', default=1e3, type=float)
+    parser.add_argument('--max_ms1_sampling_intensity', dest='max_ms1_sampling_intensity', default=1e9, type=float)
+    parser.add_argument('--formula_database_file', dest='formula_database_file', type=str)
+    parser.add_argument('--n_chems', dest='n_chems', default=10, type=int)
+    parser.add_argument('--ms_levels', dest='ms_levels', default=2, type=int)
+    parser.add_argument('--output_msp_file', dest='output_msp_file', default=None, type=str)
+    parser.add_argument('--spike_max', dest='spike_max', default=1000, type=float)
+    parser.add_argument('--output_swath_file', dest='output_swath_file', type=str, default=None)
+    parser.add_argument('--print_chems', dest='print_chems', action='store_true')
+
+
+    args = parser.parse_args()
+
+    formula_database = load_obj(args.formula_database_file)
+
+    logger.debug("Loaded {} formulas".format(len(formula_database)))
+
+    fs = DatabaseFormulaSampler(formula_database, min_mz=args.min_mz, max_mz=args.max_mz)
+
+    
+
+    ri = UniformRTAndIntensitySampler(min_rt=args.min_rt, max_rt=args.max_rt, min_log_intensity=np.log(args.min_ms1_sampling_intensity), max_log_intensity=np.log(args.max_ms1_sampling_intensity))
+    cs = UniformMS2Sampler()
+
+    cm = ChemicalMixtureCreator(fs, rt_and_intensity_sampler=ri, ms2_sampler=cs, adduct_prior_dict=ADDUCT_DICT_POS_MH)
+
+
+    dataset = cm.sample(args.n_chems, args.ms_levels)
+
+    if args.print_chems:
+        logger.debug("Sampled chems")
+        for chem in dataset:
+            logger.debug(chem)
+
+
+    if args.output_msp_file is not None:
+        write_msp(dataset, args.output_msp_file)
+
+    spike_noise = UniformSpikeNoise(0.01, args.spike_max)
+
+    ms = IndependentMassSpectrometer(POSITIVE_IONISATION_MODE, dataset, None, spike_noise=spike_noise)
+
+    controller = TopNController(POSITIVE_IONISATION_MODE, 10, 0.7, 0.01, 15, 1e3)
+
+    env = Environment(ms, controller, min_time=args.min_rt-50,max_time=args.max_rt+50)
+
+    set_log_level_warning()
+    env.run()
+
+    env.write_mzML(None, args.output_mzml_file)
+
+    if args.output_swath_file is not None:
+        sw = SWATH(args.min_mz, args.max_mz, 100, 0.0)
+        ms = IndependentMassSpectrometer(POSITIVE_IONISATION_MODE, dataset, None, spike_noise=spike_noise)
+        env = Environment(ms, sw, min_time=args.min_rt-50,max_time=args.max_rt+50)
+        env.run()
+        env.write_mzML(None, args.output_swath_file)
+
+
---
+++ vimms/scripts/parse_txt.py
@@ -0,0 +1,141 @@
+# parse the .txt output
+import argparse
+import re
+from datetime import datetime
+import pylab as plt
+
+def get_running_number(line):
+    m = re.search('runningNumber=(\d+)', line)
+    if m is None:
+        return -1
+    else:
+        return int(m.group(1))
+
+class SequenceItem(object):
+    def __init__(self,runningNumber):
+        self.running_number = runningNumber
+        self.ms_level = None
+        self.start_time = None
+        self.inject_time = None
+    def add_send(self,send_time):
+        self.send_time = send_time
+    def add_receive(self,receive_time):
+        self.receive_time = receive_time
+        self.duration = self.receive_time - self.send_time
+    def __str__(self):
+        return "{}, {}: {}, {}".format(self.running_number,self.ms_level,self.duration.total_seconds(),60*self.start_time)
+
+def get_time(line):
+    time_token = line.split()[0]
+    dd = datetime.strptime(time_token,'[%H:%M:%S.%f]')
+    return dd
+
+def get_next_ms_level(lines,pos):
+    while 'Key = ScanType' not in lines[pos]:
+        pos += 1
+    if 'Full' in lines[pos]:
+        return 1
+    else:
+        return 2
+
+def get_next_start_time(lines,pos):
+    while 'Key = StartTime' not in lines[pos]:
+        pos += 1
+    tokens = lines[pos].split('=')
+    return float(tokens[2])
+
+def get_next_inject_time(lines,pos):
+    while 'Key = InjectTime' not in lines[pos]:
+        pos += 1
+    tokens = lines[pos].split('=')
+    return float(tokens[2])
+
+def get_previous_can_accept(lines,pos):
+    while 'CanAcceptNextCustomScan' not in lines[pos]:
+        pos -= 1
+    return get_time(lines[pos])
+
+
+def extract_scan_sequence(lines, start_no=10000):
+    scan_no = start_no
+    scan_sequence_dict = {}
+    for pos,line in enumerate(lines):
+        if 'Placing' in line:
+            # get the running number
+            running_number = get_running_number(line)
+            if running_number < start_no:
+                continue
+            if not running_number in scan_sequence_dict:
+                scan_sequence_dict[running_number] = SequenceItem(running_number)
+            scan_sequence_dict[running_number].add_send(get_time(line))
+            scan_sequence_dict[running_number].ms_level = get_next_ms_level(lines,pos)
+            scan_sequence_dict[running_number].fusion_accept = get_previous_can_accept(lines,pos)
+        if 'Received' in line:
+            running_number = get_running_number(line)
+            if running_number < start_no:
+                continue
+            if not running_number in scan_sequence_dict:
+                scan_sequence_dict[running_number] = SequenceItem(running_number)
+            scan_sequence_dict[running_number].add_receive(get_time(line))
+            scan_sequence_dict[running_number].start_time = get_next_start_time(lines,pos)
+            scan_sequence_dict[running_number].inject_time = get_next_inject_time(lines,pos)
+
+    scan_sequence_list = list(scan_sequence_dict.values())
+    scan_sequence_list.sort(key = lambda x: x.running_number)
+    return scan_sequence_list
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Parse the log output')
+    parser.add_argument('--plot_from',dest='plot_from',type=int,default=None)
+    parser.add_argument('--plot_to',dest='plot_to',type=int,default=None)
+    parser.add_argument('txt_file',type=str)
+
+    args = parser.parse_args()
+
+    lines = []
+    with open(args.txt_file,'r') as f:
+        for line in f:
+            lines.append(line)
+    print("Loaded {} lines".format(len(lines)))
+
+    scan_sequence_list = extract_scan_sequence(lines)
+
+    start_time = scan_sequence_list[0].send_time
+    if args.plot_from is not None or args.plot_to is not None:
+        if args.plot_from is None:
+            plot_from = 0
+        else:
+            plot_from = args.plot_from
+        if args.plot_to is None:
+            plot_to = len(scan_sequence_list)
+        else:
+            plot_to = args.plot_to
+
+        plot_list = list(filter(lambda x: x.running_number >= plot_from and x.running_number <= plot_to, scan_sequence_list))
+        for s in plot_list:
+            print(s)
+
+        plt.figure()
+        for i,s in enumerate(plot_list):
+            if s.ms_level == 1:
+                col = 'k'
+            else:
+                col = 'r'
+            y = s.running_number
+            plt.plot([s.send_time,s.receive_time],[y,y],col)
+            text_string = "{} (i{:.0f})".format(str(s.duration.total_seconds()),s.inject_time)
+            
+            if i < len(plot_list) - 1:
+                # dd = s.send_time - plot_list[i+1].send_time
+                ff = 60.0*(plot_list[i+1].start_time - s.start_time)
+                if s.ms_level == 2 and ff > 0.3:
+                    print(s.running_number)
+                # gg = s.receive_time - plot_list[i+1].receive_time
+                text_string += " ({:.3f})".format(ff)
+            plt.text(s.send_time,y+0.1,text_string)
+        plt.title(args.txt_file)
+        yl = plt.ylim()
+        plt.ylim(yl[0],yl[1]+1)
+        plt.grid()
+        plt.show()
+        

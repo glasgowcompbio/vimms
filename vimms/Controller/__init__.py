5df8988f909a8f4859921f59b3eb018b9486e0ec
VinnyDavies <vinny.davies@glasgow.ac.uk>
2020-09-03 14:28:16 +0100

    updated controller loading

--- vimms/Controller/__init__.py
+++ vimms/Controller/__init__.py
@@ -4,4 +4,3 @@ from vimms.Controller.fullscan import *
 from vimms.Controller.misc import *
 from vimms.Controller.roi import *
 from vimms.Controller.topN import *
-from vimms.Controller.tree import *

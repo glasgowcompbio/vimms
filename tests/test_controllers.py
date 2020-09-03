29a7e1866fa5a8bf28b939dfa63dfebe5db6acee
Simon Rogers <simon.d.rogers@gmail.com>
2020-09-03 17:23:53 +0100

    added msdial experiment  file writing method to SWATH. Needs to be done for AIF

--- tests/test_controllers.py
+++ tests/test_controllers.py
@@ -1,4 +1,5 @@
 import unittest
+import csv
 from pathlib import Path
 
 import pytest
@@ -923,6 +924,24 @@ class TestSWATH:
         assert len(ms2_scans3[0].mzs) == sum([len(c.children) for c in even_chems])
         assert len(ms2_scans3[0].mzs) == sum([len(s.mzs) for s in ms2_scans2[:2]])
 
+    def test_swath_msdial_experiment_file(self):
+        min_mz = 50
+        max_mz = 440
+        width = 100
+        scan_overlap = 0
+        controller = SWATH(min_mz, max_mz, width, scan_overlap=scan_overlap)
+        out_file = Path(OUT_DIR, 'swath_experiment.txt')
+        controller.write_msdial_experiment_file(out_file)
+
+        assert os.path.exists(out_file)
+        with open(out_file, 'r') as f:
+            reader = csv.reader(f, delimiter='\t')
+            rows = []
+            for row in reader:
+                rows.append(row)
+        expected_row = ['4','SWATH','350','450']
+        for i,val in  enumerate(expected_row):
+            assert rows[-1][i] == val
 
 class TestFixedScansController:
     """

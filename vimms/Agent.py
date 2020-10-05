48f00bbaf4f732fc34d2ffbbeb2fe3a4a8e4c9d3
Joe Wandy <joewandy@gmail.com>
2020-10-05 12:47:45 +0100

    Renamed oracle to agent

--- vimms/Agent.py
+++ vimms/Agent.py
@@ -1,21 +1,24 @@
-# Oracle.py
+# Agent.py
 import numpy as np
-from loguru import logger
-from vimms.Exclusion import BoxHolder, ExclusionItem
+
 from vimms.Common import ScanParameters
+from vimms.Exclusion import BoxHolder, ExclusionItem
 
 
-# some example oracles...
+# some example agents...
 
-class AbstractOracle(object):
+class AbstractAgent(object):
     def __init__(self):
         self.task_list = []
+
     def next_tasks(self, scan, controller, current_task_id):
         raise NotImplementedError
+
     def update(self, last_scan, controller):
         raise NotImplementedError
 
-class FullScanOracle(AbstractOracle):
+
+class FullScanAgent(AbstractAgent):
     def next_tasks(self, scan, controller, current_task_id):
         new_tasks = []
         ms1_scan_params = controller.get_ms1_scan_params()
@@ -27,8 +30,9 @@ class FullScanOracle(AbstractOracle):
 
     def update(self, last_scan, controller):
         pass
-    
-class TopNOracle(AbstractOracle):
+
+
+class TopNAgent(AbstractAgent):
     def __init__(self, N, min_ms1_intensity):
         super().__init__()
         self.N = N
@@ -46,7 +50,6 @@ class TopNOracle(AbstractOracle):
         rt = scan.rt
         idx = np.argsort(intensities)[::-1]
 
-
         fragmented_count = 0
         for i in idx:
             if fragmented_count >= self.N:
@@ -56,35 +59,36 @@ class TopNOracle(AbstractOracle):
 
             if intensity < self.min_ms1_intensity:
                 break
-        
-            if self.exclusion.is_in_box(mz, rt): # will always return false in this controller, but used in children
+
+            if self.exclusion.is_in_box(mz, rt):  #  will always return false in this controller, but used in children
                 continue
 
             precursor_scan_id = scan.scan_id
             dda_scan_params = controller.get_ms2_scan_params(mz, intensity, precursor_scan_id, self.isolation_width,
-                                                        self.mz_tol, self.rt_tol)
+                                                             self.mz_tol, self.rt_tol)
             new_tasks.append(dda_scan_params)
             self.task_list.append((controller, dda_scan_params))
             fragmented_count += 1
             current_task_id += 1
-        
+
         ms1_scan_params = controller.get_ms1_scan_params()
         new_tasks.append(ms1_scan_params)
         self.task_list.append((controller, ms1_scan_params))
         current_task_id += 1
         next_processed_scan_id = current_task_id
-        
+
         return new_tasks, current_task_id, next_processed_scan_id
-    
+
     def update(self, last_scan, controller):
         pass
 
-class TopNDEWOracle(TopNOracle):
+
+class TopNDEWAgent(TopNAgent):
     def __init__(self, N, min_ms1_intensity, mz_tol, rt_tol):
         super().__init__(N, min_ms1_intensity)
         self.mz_tol = mz_tol
         self.rt_tol = rt_tol
-    
+
     def update(self, last_scan, controller):
         rt = last_scan.rt
         if last_scan.ms_level >= 2:  # if ms-level is 2, it's a custom scan and we should always know its scan parameters
@@ -104,7 +108,5 @@ class TopNDEWOracle(TopNOracle):
                 rt_lower = rt - self.rt_tol
                 rt_upper = rt + self.rt_tol
                 x = ExclusionItem(from_mz=mz_lower, to_mz=mz_upper, from_rt=rt_lower, to_rt=rt_upper,
-                          frag_at=rt)
+                                  frag_at=rt)
                 self.exclusion.add_box(x)
-        
-        

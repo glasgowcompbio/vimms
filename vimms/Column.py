d171411e44b3f0403d7d2489a68764254247d45c
VinnyDavies <vinny.davies@glasgow.ac.uk>
2020-10-16 16:55:39 +0100

    Initial column code

---
+++ vimms/Column.py
@@ -0,0 +1,104 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# class Column(object):
+#     def __init__(self, dataset, param_dict):
+#         self.dataset = dataset
+#         self.drift = self.get_drift(param_dict)
+#         self.adjusted_dataset = self.rt_adjustment()
+#
+#     def _get_drift(self, param_dict):
+#         NotImplementedError()
+#
+#     def rt_adjustment(selfsarezxd):
+#         NotImplementedError()
+#
+#     def get_dataset(self):
+#         return self.adjusted_dataset
+#
+#     def get_chemical(self, idx):
+#         return self.adjusted_dataset[idx]
+#
+#     def plot_drift(self):
+#         chem_rts = [chem.rt for chem in self.dataset]
+#         rt_range = (min(chem_rts), max(chem_rts))
+#         NotImplementedError()
+#
+#     def plot_drift_distribution(self):
+#         NotImplementedError() # plot samples of the drift function based on the param_dicts
+#
+#
+# class CleanColumn(Column):
+#     def __init__(self, dataset):
+#         super().__init__(dataset, None)
+#
+#     def rt_adjustment(self, param_dict):
+#         return self.dataset
+#
+#
+# class GaussianProcessColumn(Column):
+#     def __init__(self, dataset, rbf_params, intercept_params, linear_params):
+#         # construct a param_dict
+#         param_dict = rbf_params
+#         super().__init__(dataset, param_dict)
+#
+#     def rt_adjustment(self, param_dict):
+#
+#         NotImplementedError()
+#
+#     def get_gp(self):
+
+# class CleanColumn
+
+# class DirtyColumn
+
+
+class GaussianProcessColumn(object):
+    def __init__(self, dataset, rbf_params, intercept_params, linear_params, noise_sd):
+        self.dataset = dataset
+        self.dataset_rts = np.array([chem.rt for chem in self.dataset])
+        self.rbf_params = rbf_params
+        self.intercept_params = intercept_params
+        self.linear_params = linear_params
+        self.noise_sd = noise_sd
+        self.offsets, self.true_drift_function = self._get_offsets()
+
+    def _get_offsets(self):
+        intercept_term = np.random.normal(self.intercept_params[0], self.intercept_params[1])
+        linear_term = np.random.normal(self.linear_params[0], self.linear_params[1])
+        mean = intercept_term + linear_term * self.dataset_rts
+        return self._draw_offset(mean)
+
+    def _draw_offset(self, mean):
+        N = len(self.dataset_rts)
+        K = np.zeros((N, N), np.double)
+        for n in range(N):
+            for m in range(N):
+                K[n, m] = self.rbf_params[0] * np.exp(-(1. / self.rbf_params[1]) * (self.dataset_rts[n] - self.dataset_rts[m]) ** 2)
+        true_offset_function = np.random.multivariate_normal(mean, K)
+        offsets = true_offset_function + np.random.normal(0, self.noise_sd, N)
+        return offsets, true_offset_function
+
+    def get_dataset(self):
+        return self.dataset + self.offsets
+
+    def get_chemical(self, idx):
+        return self.dataset[idx] + self.offsets[idx]
+
+    def plot_drift(self):
+        order = np.argsort(self.dataset_rts)
+        plt.figure(figsize=(12, 8))
+        plt.plot(self.dataset_rts[order], self.true_drift_function[order], 'b')
+        plt.plot(self.dataset_rts[order], self.true_drift_function[order] + 1.95 * self.noise_sd, 'b--')
+        plt.plot(self.dataset_rts[order], self.true_drift_function[order] - 1.95 * self.noise_sd, 'b--')
+        plt.plot(self.dataset_rts, self.offsets, 'ro')
+        plt.show()
+
+    def plot_drift_distribution(self):
+        order = np.argsort(self.dataset_rts)
+        plt.figure(figsize=(12, 8))
+        for i in range(100):
+            offsets, true_drift_function = self._get_offsets()
+            plt.plot(self.dataset_rts[order], true_drift_function[order])
+        plt.show()
+

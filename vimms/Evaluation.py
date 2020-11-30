1884bf230962e23eddef66cb4a975097180588ce
VinnyDavies <vinny.davies@glasgow.ac.uk>
2020-11-30 14:24:47 +0000

    improved evaluation

--- vimms/Evaluation.py
+++ vimms/Evaluation.py
@@ -4,7 +4,7 @@ from mass_spec_utils.data_import.mzmine import load_picked_boxes,map_boxes_to_sc
 import numpy as np
 
 
-def evaluate_simulated_env(env):
+def evaluate_simulated_env(env, base_chemicals=None):
     chems = env.mass_spec.chemicals
     max_coverage = len(chems)
     max_coverage_intensity = sum([chem.max_intensity for chem in chems])
@@ -18,7 +18,14 @@ def evaluate_simulated_env(env):
                 coverage_intensity[chem_idx] = max(coverage_intensity[chem_idx], event.parents_intensity[0])
     coverage_prop = sum(coverage) / max_coverage
     coverage_intensity_prop = sum(coverage_intensity) / max_coverage_intensity
-    return coverage, coverage_intensity, coverage_prop, coverage_intensity_prop
+    chemicals_fragmented = np.array(chems)[np.where(coverage == 1)]
+    if base_chemicals is not None:
+        base_chemicals_coverage = [(chem in chemicals_fragmented)*1 for chem in base_chemicals]
+    else:
+        base_chemicals_coverage = None
+    return {'coverage': coverage, 'coverage_intensity': coverage_intensity, 'coverage_prop': coverage_prop,
+            'coverage_intensity_prop': coverage_intensity_prop, 'chemicals_fragmented': chemicals_fragmented,
+            'base_chemicals_coverage': base_chemicals_coverage}
 
 
 def evaluate_mzml(mzml_file, picked_peaks_file, half_isolation_window):

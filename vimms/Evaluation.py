13e5bbacabe365a2a93ca3988fea95a117126f3c
VinnyDavies <vinny.davies@glasgow.ac.uk>
2020-12-17 11:32:28 +0000

    added loading for xcms and peakonly

--- vimms/Evaluation.py
+++ vimms/Evaluation.py
@@ -1,7 +1,8 @@
 from mass_spec_utils.data_import.mzml import MZMLFile
-from mass_spec_utils.data_import.mzmine import load_picked_boxes,map_boxes_to_scans
+from mass_spec_utils.data_import.mzmine import load_picked_boxes, map_boxes_to_scans, PickedBox
 
 import numpy as np
+import csv
 
 
 def evaluate_simulated_env(env, base_chemicals=None):
@@ -40,4 +41,31 @@ def evaluate_mzml(mzml_file, picked_peaks_file, half_isolation_window):
     scans2boxes, boxes2scans = map_boxes_to_scans(mz_file, boxes, half_isolation_window=half_isolation_window)
     coverage = len(boxes2scans)
     return coverage
+
+
+def load_xcms_boxes(box_file):
+    boxes = load_picked_boxes(box_file)
+    for box in boxes:
+        box.rt_in_seconds /= 60.
+        box.rt_range_in_seconds = [r/60. for r in box.rt_range_in_seconds]
+        box.rt /= 60.
+        box.rt_range = [r/60. for r in box.rt_range]
+    return boxes
+
+
+def load_peakonly_boxes(box_file):
+    boxes = []
+    with open(box_file,'r') as f:
+        reader = csv.reader(f)
+        heads = next(reader)
+        for line in reader:
+            peak_id = int(line[0])
+            peak_mz = float(line[1])
+            rt_min = 60.0*float(line[2])
+            rt_max = 60.0*float(line[3])
+            height = float(line[4])
+            new_box = PickedBox(peak_id,peak_mz,rt_max+rt_min/2,peak_mz,peak_mz,rt_min,rt_max,height = height)
+            boxes.append(new_box)
+    boxes.sort(key=lambda x: x.rt)
+    return boxes
 

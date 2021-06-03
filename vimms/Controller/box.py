5c4a7587bdc7c5ee038e4b3434e6936fb647637a
VinnyDavies <vinny.davies@glasgow.ac.uk>
2021-06-03 10:44:49 +0100

    controllers

--- vimms/Controller/box.py
+++ vimms/Controller/box.py
@@ -157,3 +157,78 @@ class IntensityNonOverlapController(GridController):
             r.to_box(self.min_rt_width, self.min_mz_width, rt_shift=(-fn(r)[0])), self.current_roi_intensities[i]) for
                          i, r in enumerate(self.live_roi)])
         return self._get_top_N_scores(scores * self._score_filters())
+
+
+class FlexibleNonOverlapController(GridController):
+    def _get_scores(self):
+        fn = self.grid.get_estimator()
+        scores = [self.grid.flexible_non_overlap(
+            r.to_box(self.min_rt_width, self.min_mz_width, rt_shift=(-fn(r)[0])), self.current_roi_intensities[i]) for
+                         i, r in enumerate(self.live_roi)]
+        return self._get_top_N_scores(scores * self._score_filters())
+
+
+class TopNBoxRoiController2(GridController):
+    def __init__(self, ionisation_mode, isolation_width, mz_tol, min_ms1_intensity, min_roi_intensity, min_roi_length,
+                 N, grid, boxes_params=None, boxes=None, boxes_intensity=None, boxes_pvalues=None, rt_tol=10,
+                 min_roi_length_for_fragmentation=1, length_units="scans", ms1_shift=0, min_rt_width=0.01,
+                 min_mz_width=0.01, params=None, box_min_rt_width=0.01, box_min_mz_width=0.01):
+        self.boxes_params = boxes_params
+        self.boxes = boxes
+        self.boxes_intensity = boxes_intensity  # the intensity the boxes have been fragmented at before
+        self.boxes_pvalues = boxes_pvalues
+        self.box_min_rt_width = box_min_rt_width
+        self.box_min_mz_width = box_min_mz_width
+        super().__init__(ionisation_mode, isolation_width, mz_tol, min_ms1_intensity, min_roi_intensity, min_roi_length,
+                         N, grid=grid, rt_tol=rt_tol, min_roi_length_for_fragmentation=min_roi_length_for_fragmentation,
+                         length_units=length_units, ms1_shift=ms1_shift, min_rt_width=min_rt_width,
+                         min_mz_width=min_mz_width, params=params)
+
+    def _get_scores(self):
+        if self.boxes is not None:
+            # calculate dda stuff
+            log_intensities = np.log(self.current_roi_intensities)
+            intensity_filter = (np.array(self.current_roi_intensities) > self.min_ms1_intensity)
+            time_filter = (1 - np.array(self.live_roi_fragmented).astype(int))
+            time_filter[time_filter == 0] = (
+                    (self.scan_to_process.rt - np.array(self.live_roi_last_rt)[time_filter == 0]) > self.rt_tol)
+            # calculate overlap stuff
+            initial_scores = []
+            copy_boxes = deepcopy(self.boxes)
+            for box in copy_boxes:
+                box.pt2.x = min(box.pt2.x, max(self.last_ms1_rt, box.pt1.x))
+            prev_intensity = np.maximum(np.log(np.array(self.boxes_intensity)), [0 for i in self.boxes_intensity])
+            box_fragmented = (np.array(self.boxes_intensity) == 0) * 1
+            for i in range(len(log_intensities)):
+                overlaps = np.array(self.live_roi[i].get_boxes_overlap(copy_boxes, self.box_min_rt_width,
+                                                                       self.box_min_mz_width))
+                # new peaks not in list of boxes
+                new_peaks_score = max(0, (1 - sum(overlaps))) * log_intensities[i]
+                # previously fragmented peaks
+                old_peaks_score1 = sum(overlaps * (log_intensities[i] - prev_intensity) * (1 - box_fragmented))
+                # peaks seen before, but not fragmented
+                old_peaks_score2 = sum(overlaps * log_intensities[i] * box_fragmented)
+                if self.boxes_pvalues is not None:
+                    # based on p values, previously fragmented
+                    p_value_scores1 = sum(
+                        overlaps * (log_intensities[i] - prev_intensity) * (1 - np.array(self.boxes_pvalues)))
+                    # based on p values, not previously fragmented
+                    p_value_scores2 = sum(overlaps * log_intensities[i] * (1 - np.array(self.boxes_pvalues)))
+                # get the score
+                score = self.boxes_params['theta1'] * new_peaks_score
+                score += self.boxes_params['theta2'] * old_peaks_score1
+                score += self.boxes_params['theta3'] * old_peaks_score2
+                if self.boxes_pvalues is not None:
+                    score += self.boxes_params['theta4'] * p_value_scores1
+                    score += self.boxes_params['theta5'] * p_value_scores2
+                score *= time_filter[i]
+                score *= intensity_filter  # check intensity meets minimal requirement
+                score *= (score > self.boxes_params['min_score'])  # check meets min score
+                initial_scores.append(score[0])
+            initial_scores = np.array(initial_scores)
+        else:
+            initial_scores = self._get_dda_scores()
+
+        scores = self._get_top_N_scores(initial_scores)
+        return scores
+

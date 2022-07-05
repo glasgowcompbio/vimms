36cf1ac9dbc7b668b9e89eccee17c5db18c045a9
Joe Wandy <joewandy@gmail.com>
2022-07-05 12:24:56 +0100

    Added some methods to generate sequence CSV file (to use when running on the Fusion).

--- vimms/scripts/multi_sample_experiment.py
+++ vimms/scripts/multi_sample_experiment.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from loguru import logger
 
 from vimms.Agent import TopNDEWAgent
@@ -45,7 +46,8 @@ def run_batch(initial_runs, controller_repeat, experiment_params, samples,
             agent = make_agent(experiment_params)
         elif controller_name in [CONTROLLER_NON_OVERLAP, CONTROLLER_HARD_ROI_EXCLUSION]:
             grid = make_grid(experiment_params, False)
-        elif controller_name in [CONTROLLER_INTENSITY_NON_OVERLAP, CONTROLLER_INTENSITY_ROI_EXCLUSION]:
+        elif controller_name in [CONTROLLER_INTENSITY_NON_OVERLAP,
+                                 CONTROLLER_INTENSITY_ROI_EXCLUSION]:
             grid = make_grid(experiment_params, True)
 
         # find how many replicates have been specified
@@ -61,7 +63,8 @@ def run_batch(initial_runs, controller_repeat, experiment_params, samples,
                 controller = select_controller(controller_name, experiment_params, agent, grid)
                 out_file = get_out_file(controller_name, sample, i)
                 run_controller(use_instrument, ref_dir, dataset, scan_duration_dict,
-                               pbar, max_time, ionisation_mode, use_column, controller, out_dir, out_file)
+                               pbar, max_time, ionisation_mode, use_column, controller, out_dir,
+                               out_file)
 
 
 # a variant of run_batch but for exhaustive fragmentation (experiment 3)
@@ -95,7 +98,8 @@ def run_batch_exhaustive(initial_runs, controller_repeat, experiment_params, sam
                 agent = make_agent(experiment_params)
             elif controller_name in [CONTROLLER_NON_OVERLAP, CONTROLLER_HARD_ROI_EXCLUSION]:
                 grid = make_grid(experiment_params, False)
-            elif controller_name in [CONTROLLER_INTENSITY_NON_OVERLAP, CONTROLLER_INTENSITY_ROI_EXCLUSION]:
+            elif controller_name in [CONTROLLER_INTENSITY_NON_OVERLAP,
+                                     CONTROLLER_INTENSITY_ROI_EXCLUSION]:
                 grid = make_grid(experiment_params, True)
 
             for i in range(repeat):
@@ -103,7 +107,8 @@ def run_batch_exhaustive(initial_runs, controller_repeat, experiment_params, sam
                 controller = select_controller(controller_name, experiment_params, agent, grid)
                 out_file = get_out_file(controller_name, sample, i)
                 run_controller(use_instrument, ref_dir, dataset, scan_duration_dict,
-                               pbar, max_time, ionisation_mode, use_column, controller, out_dir, out_file)
+                               pbar, max_time, ionisation_mode, use_column, controller, out_dir,
+                               out_file)
 
 
 def make_grid(experiment_params, split_grid):
@@ -203,11 +208,120 @@ def run_controller(use_instrument, ref_dir, dataset, scan_duration_dict,
     if use_instrument:
         from vimms_fusion.MassSpec import IAPIMassSpectrometer
         from vimms_fusion.Environment import IAPIEnvironment
-        mass_spec = IAPIMassSpectrometer(ionisation_mode, ref_dir, filename=None, show_console_logs=False,
+        mass_spec = IAPIMassSpectrometer(ionisation_mode, ref_dir, filename=None,
+                                         show_console_logs=False,
                                          use_column=use_column)
-        env = IAPIEnvironment(mass_spec, controller, max_time, progress_bar=pbar, out_dir=out_dir, out_file=out_file)
+        env = IAPIEnvironment(mass_spec, controller, max_time, progress_bar=pbar, out_dir=out_dir,
+                              out_file=out_file)
     else:
-        mass_spec = IndependentMassSpectrometer(ionisation_mode, dataset, scan_duration=scan_duration_dict)
-        env = Environment(mass_spec, controller, 0, max_time, progress_bar=pbar, out_dir=out_dir, out_file=out_file)
+        mass_spec = IndependentMassSpectrometer(ionisation_mode, dataset,
+                                                scan_duration=scan_duration_dict)
+        env = Environment(mass_spec, controller, 0, max_time, progress_bar=pbar, out_dir=out_dir,
+                          out_file=out_file)
 
     env.run()
+
+
+def generate_sequence_df(initial_runs, controller_repeat, samples, position, raw_output_path,
+                         blank_method_path, instrument_method_path, exhaustive=False):
+    all_runs = []
+
+    for sample in initial_runs:
+        method_file, sample_type = select_sample_type_and_method_file(
+            blank_method_path, instrument_method_path, sample)
+        sample_position = position[sample]
+        controller_name = CONTROLLER_FULLSCAN
+        out_file = "{}_{}".format(controller_name, sample)
+        row = generate_row(method_file, out_file, raw_output_path, sample_position, sample_type)
+        all_runs.append(row)
+
+    if not exhaustive:  # normal setup
+
+        for controller_name in controller_repeat:
+            sample_to_process, repeat = controller_repeat[controller_name]
+            for i in range(repeat):
+
+                for sample in samples:
+                    if sample not in sample_to_process:
+                        continue
+
+                    method_file, sample_type = select_sample_type_and_method_file(
+                        blank_method_path, instrument_method_path, sample)
+                    sample_position = position[sample]
+                    out_file = "{}_{}_{}".format(controller_name, sample, i)
+                    row = generate_row(method_file, out_file, raw_output_path, sample_position,
+                                       sample_type)
+                    all_runs.append(row)
+
+    else:  # exhaustive setup
+
+        for controller_name in controller_repeat:
+            sample_to_process, repeat = controller_repeat[controller_name]
+            for sample in samples:
+                if sample not in sample_to_process:
+                    continue
+
+                for i in range(repeat):
+                    method_file, sample_type = select_sample_type_and_method_file(
+                        blank_method_path, instrument_method_path, sample)
+                    sample_position = position[sample]
+                    out_file = "{}_{}_{}".format(controller_name, sample, i)
+                    row = generate_row(method_file, out_file, raw_output_path, sample_position,
+                                       sample_type)
+                    all_runs.append(row)
+
+    headers = ['Sample Type', 'File Name', 'Sample ID', 'Path', 'Instrument Method',
+               'Process Method', 'Calibration File', 'Position', 'Inj Vol',
+               'Level', 'Sample Wt', 'Sample Vol', 'ISTD Amt', 'Dil Factor',
+               'L1 Study', 'L2 Client', 'L3 Laboratory', 'L4 Company', 'L5 Phone', 'Comment',
+               'Sample Name']
+    df = pd.DataFrame(all_runs, columns=headers)
+    return df
+
+
+def select_sample_type_and_method_file(blank_method_path, instrument_method_path, sample):
+    if 'blank' in sample.lower():
+        sample_type = 'Blank'
+        method_file = blank_method_path
+    else:
+        sample_type = 'Unknown'
+        method_file = instrument_method_path
+    return method_file, sample_type
+
+
+def generate_row(method_file, out_file, raw_output_path, sample_position, sample_type):
+    sample_id = 1
+    process_method = None
+    calibration_file = None
+    injection_vol = 10
+    level = None
+    sample_wt = 0
+    sample_vol = 0
+    istd_amt = 0
+    dil_factor = 1
+    l1_study = None
+    l2_client = None
+    l3_laboratory = None
+    l4_company = None
+    l5_phone = None
+    comment = None
+    sample_name = None
+    row = [sample_type, out_file, sample_id, raw_output_path, method_file,
+           process_method, calibration_file, sample_position, injection_vol,
+           level, sample_wt, sample_vol, istd_amt, dil_factor,
+           l1_study, l2_client, l3_laboratory, l4_company, l5_phone, comment, sample_name]
+    return row
+
+
+def write_sequence_csv(df, out_file):
+    with open(out_file, 'w') as f:
+        df.to_csv(f, header=True, index=False)
+    line_prepender(out_file, 'Bracket Type=4,')
+
+
+def line_prepender(filename, line):
+    # https://stackoverflow.com/questions/5914627/prepend-line-to-beginning-of-a-file
+    with open(filename, 'r+') as f:
+        content = f.read()
+        f.seek(0, 0)
+        f.write(line.rstrip('\r\n') + '\n' + content)
